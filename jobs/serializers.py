93f8cdaa5056a332d211f1c2809c67908b181fa3
Naman-vefogix <namankalwani.vefogix@gmail.com>
2026-05-23 10:21:42 +0530

    feat: build REST APIs for jobs app using Django REST Framework

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
 from .models import Job
 
+# class JobSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Job
+#         fields = '__all__'
+
+
+
 class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
-        fields = '__all__'
+        fields = [
+        'id',
+        'title',
+        'description',
+        'created_at'
+        ]
+        read_only_fields = [
+            'id',
+            'created_at'
+        ]
--- jobs/views.py
+++ jobs/views.py
@@ -6,15 +6,115 @@ from notifications.services import create_notification
 
 # rest imports
 from rest_framework.response import Response
-from rest_framework.decorators import api_view  
+from rest_framework.decorators import api_view, permission_classes
 from .serializers import JobSerializer
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
 
-# rest api
+# rest imports - views
+from rest_framework.views import APIView
+
+# test postman - will explicit mention user
+from users.models import CustomUser 
+
+## rest api
+
+# APIView (used here)
+
+class JobAPIView(APIView):
+    def get(self,request):
+        jobs = Job.objects.all()
+        serializer = JobSerializer(jobs,many = True)
+        return Response(serializer.data)
+
+    def post(self,request):
+        serializer = JobSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(client = request.user)
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class DetailJobAPIView(APIView):
+    def get(self, request, id):
+        job = get_object_or_404(Job, id=id)
+        serializer = JobSerializer(job)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        job = get_object_or_404(Job, id=id)
+        serializer = JobSerializer(job,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, id):
+        job = get_object_or_404(Job, id=id)
+        serializer = JobSerializer(job,data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        job = get_object_or_404(Job, id=id)
+        job.delete()
+        return Response({"message": "Deleted successfully"},status=status.HTTP_204_NO_CONTENT)
+
+#using generics
+class JobListAPIView(generics.ListAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+class JobListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+class JobDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    lookup_field = 'id'
+
+# manual REST APIs 
 @api_view(['GET'])
 def api_job_list(request):
-    jobs = Job.objects.all()
-    serializer = JobSerializer(jobs, many = True)
+    job = Job.objects.all()
+    serializer = JobSerializer(job,many = True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def api_job_detail(request,job_id = None):
+    try:
+        job = Job.objects.get(id = job_id)
+    except Job.DoesNotExist:
+        return Response({'error':"job not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+    serializer = JobSerializer(job)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated]) #
+def api_create_job(request):
+    serializer = JobSerializer(data = request.data)
+    if serializer.is_valid():
+        dummy_user = CustomUser.objects.first() # explicit user mentioned here
+        # serializer.save(client = request.user)
+        serializer.save(client = dummy_user) # have to change dummy user here 
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PATCH'])
+def api_update_job(request, job_id):
+    try:
+        job = Job.objects.get(id = job_id)
+    except Job.DoesNotExist:
+        return Response({"error" : "job not found"}, status=status.HTTP_404_NOT_FOUND)
+    serializer = JobSerializer(job, data=request.data,partial = True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 
 # Create your views here.
