47d6fa2aa9d6fee1ed47b6a963b8b9c4a5868431
Naman-vefogix <namankalwani.vefogix@gmail.com>
2026-05-26 11:42:45 +0530

    chore: migrate database from SQLite3 to PostgreSQL

--- freelancer/settings.py
+++ freelancer/settings.py
@@ -87,11 +87,14 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'freelancer',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
-}
-
+}    
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -128,4 +131,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
